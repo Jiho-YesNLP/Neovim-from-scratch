6edd7831b1d725932e37b632dab74d07550016cf
Jiho Noh <jnoh3@kennesaw.edu>
2022-01-04 14:03:19 -0500

    options keys for Mac

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -17,6 +17,7 @@ vim.cmd [[
     autocmd!
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
+    autocmd FileType markdown lua require('cmp').setup.buffer { enabled = false }
   augroup end
 
   augroup _auto_resize
